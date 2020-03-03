abc5d0e8b0a1d6314686fea28aeb3c883661a5a1
makoto <makoto@makoto-no-MacBook-Pro.local>
2020-03-03 11:52:52 +0900

    add

---
+++ init.el
@@ -0,0 +1,15 @@
+;; タブにスペースを使用する
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; 改行コードを表示する
+(setq eol-mnemonic-dos "(CRLF)")
+(setq eol-mnemonic-mac "(CR)")
+(setq eol-mnemonic-unix "(LF)")
+
+;; Ctrl-h -> BackSpace
+(global-set-key "\C-h" `delete-backward-char)
+
+;;; *.~ とかのバックアップファイルを作らない
+(setq make-backup-files nil)
+;;; .#* とかのバックアップファイルを作らない
+(setq auto-save-default nil)
